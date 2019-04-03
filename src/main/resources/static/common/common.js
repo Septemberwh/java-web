39e611414f4141c9f04071464a94d129b06aacac
wanghao <312939906@qq.com>
2019-04-03 15:36:04 +0800

    commint

---
+++ src/main/resources/static/common/common.js
@@ -0,0 +1,9 @@
+/**
+ * 公用的js和css
+ */
+document.write('<link href="https://cdn.bootcss.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">');
+
+<!-- Bootstrap core JavaScript -->
+<!-- Placed at the end of the document so the pages load faster -->
+document.write('<script type="text/javascript" src="https://cdn.bootcss.com/jquery/1.12.4/jquery.min.js"></script>');
+document.write('<script type="text/javascript" src="https://cdn.bootcss.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>');
